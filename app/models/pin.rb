39417484bf0b32d41df92d83bb68dc63dc33813f
Kevin Kantz <kkantz@myyouq.com>
2015-06-05 17:46:58 -0700

    added pagination to pins index

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
      belongs_to :user
-     has_attached_file :image, :styles => { :original => "800x800>", :medium => "300x300>", :thumb => "100x100>" }
+     has_attached_file :image, :styles => { :original => "600x600>", :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
