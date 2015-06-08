9d4cc17d91aaf50406c7a78d1a1dbb91b89dfa57
Kevin Kantz <kkantz@myyouq.com>
2015-06-08 11:40:38 -0700

    Added validation and name to DB and reg

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,5 @@ class Pin < ActiveRecord::Base
      belongs_to :user
      has_attached_file :image, :styles => { :original => "600x600>", :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+	validates :description, presence: true
 end
