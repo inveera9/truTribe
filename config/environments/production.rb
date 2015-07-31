609be75e3671867480f8e7ecb86c55b6b9f0b31e
Prashant Vardhan <prashant.vardhan@inveera.us>
2015-07-31 15:07:36 +0530

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,17 +76,28 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
- # Required for Heroku
-  config.action_mailer.default_url_options = { :host => 'kantz.co' }
+ 
 
-# Sets Paperclip to upload images to Amazon S3
+  # Sets Paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
-}
+  # Required for Heroku
+  config.action_mailer.default_url_options = { :host => 'http://inv-tribe.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+     :authentication => :plain,
+     :address => ENV['MAIL_ADDRESS'],
+     :port => ENV['MAIL_PORT'],
+     :user_name => ENV['MAIL_USERNAME'],
+     :password => ENV['MAIL_PASSWORD']
+  }
 
 end
